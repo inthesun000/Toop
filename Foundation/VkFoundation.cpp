ab80bb58c8a0ebff3fad460143e4ee7504387ad0
Doov <33049431+inthesun000@users.noreply.github.com>
2018-06-24 19:38:53 +0900

    Day2, Draw a triangle - Validation layers

---
+++ Foundation/VkFoundation.cpp
@@ -0,0 +1,178 @@
+#include "VkFoundation.h"
+
+#include <iostream>
+
+#include <stdexcept>
+#include <functional>
+#include <cstdlib>
+#include <cstring>
+
+
+VkResult CreateDebugReportCallbackEXT(VkInstance instance, const VkDebugReportCallbackCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugReportCallbackEXT* pCallback)
+{
+	auto func = (PFN_vkCreateDebugReportCallbackEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugReportCallbackEXT");
+	if (func != nullptr)
+	{
+		return func(instance, pCreateInfo, pAllocator, pCallback);
+	}
+	else
+	{
+		return VK_ERROR_EXTENSION_NOT_PRESENT;
+	}
+}
+
+void DestroyDebugReportCallbackEXT(VkInstance instance, VkDebugReportCallbackEXT callback, const VkAllocationCallbacks* pAllocator)
+{
+	auto func = (PFN_vkDestroyDebugReportCallbackEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugReportCallbackEXT");
+	if (func != nullptr)
+	{
+		func(instance, callback, pAllocator);
+	}
+}
+
+/*
+class VkFoundation
+*/
+
+VkFoundation::~VkFoundation()
+{
+	CleanUp();
+}
+
+void VkFoundation::CreateInstance()
+{
+	if (enableValidationLayers && !CheckValidationLayerSupport())
+	{
+		throw std::runtime_error("validation layers requested, but not available!");
+	}
+
+	VkApplicationInfo appInfo = {};
+	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName = "Foundation";
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName = "No Engine";
+	appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion = VK_API_VERSION_1_0;
+
+	VkInstanceCreateInfo createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	createInfo.pApplicationInfo = &appInfo;
+
+	uint32_t glfwExtensionCount = 0;
+
+	auto extensions = GetRequiredExtensions();
+
+	createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+	createInfo.ppEnabledExtensionNames = extensions.data();
+
+	if (enableValidationLayers)
+	{
+		createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+		createInfo.ppEnabledLayerNames = validationLayers.data();
+	}
+	else
+	{ 
+		createInfo.enabledLayerCount = 0;
+	}
+
+	if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to create instace!");
+	}
+}
+
+void VkFoundation::InitWindow()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	window = glfwCreateWindow(Width, Height, "VulkanFoundation", nullptr, nullptr);
+}
+
+void VkFoundation::CleanUp()
+{
+	if (enableValidationLayers)
+	{
+		DestroyDebugReportCallbackEXT(instance, callback, nullptr);
+	}
+
+	vkDestroyInstance(instance, nullptr);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void VkFoundation::EventLoop()
+{
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+}
+
+bool VkFoundation::CheckValidationLayerSupport()
+{
+	uint32_t layerCount;
+	vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+	std::vector<VkLayerProperties> availableLayers(layerCount);
+	vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+	for (const char* layerName : validationLayers)
+	{
+		bool layerFound = false;
+		for (const auto& layerProperties : availableLayers)
+		{
+			if (strcmp(layerName, layerProperties.layerName) == 0)
+			{
+				layerFound = true;
+				break;
+			}
+		}
+
+		if (!layerFound)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+std::vector<const char*> VkFoundation::GetRequiredExtensions()
+{
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+	std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+	if(enableValidationLayers)
+	{
+		extensions.push_back(VK_EXT_DEBUG_REPORT_EXTENSION_NAME);
+	}	
+	return extensions;
+}
+
+void VkFoundation::SetupDebugCallback()
+{
+	if (!enableValidationLayers) return;
+
+	VkDebugReportCallbackCreateInfoEXT createInfo = {};
+	createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_REPORT_CALLBACK_CREATE_INFO_EXT;
+	createInfo.flags = VK_DEBUG_REPORT_ERROR_BIT_EXT | VK_DEBUG_REPORT_WARNING_BIT_EXT;
+	createInfo.pfnCallback = DebugCallback;
+
+	if (CreateDebugReportCallbackEXT(instance, &createInfo, nullptr, &callback) != VK_SUCCESS)
+	{
+		throw std::runtime_error("failed to set up debug callback");
+	}
+}
+
+VKAPI_ATTR VkBool32 VKAPI_CALL VkFoundation::DebugCallback(VkDebugReportFlagsEXT flags, VkDebugReportObjectTypeEXT objType, uint64_t obj, size_t location, int32_t code, const char * layerPrefix, const char * msg, void * userData)
+{
+	std::cerr << "Validation layer : " << msg << std::endl;
+
+	return VK_FALSE;
+}
--- Foundation/main.cpp
+++ Foundation/main.cpp
@@ -1,36 +1,58 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-
-#define GLFW_FORCE_RADIANS
-#define GLFW_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/mat4x4.hpp>
-#include <glm/vec4.hpp>
+//#include <vulkan/vulkan.h>
 
 #include <iostream>
+#include "VkFoundation.h"
+
+
+class DrawTriangleApp
+{
+private:
+	VkFoundation * foundation=nullptr;
+
+private:
+	void InitVulkan()
+	{
+		foundation->CreateInstance();
+		foundation->SetupDebugCallback();
+	}
+
+	void Loop()
+	{
+		foundation->EventLoop();
+	}
+
+	void CleanUp()
+	{
+		delete foundation;
+	}
+
+	void InitWindow()
+	{	
+		foundation = new VkFoundation;
+	}
+
+public:
+	void Run()
+	{
+		InitWindow();
+		InitVulkan();
+		Loop();
+		CleanUp();
+	}
+};
 
 int main()
 {
-	glfwInit();
+	DrawTriangleApp app;
 
-	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	GLFWwindow* window = glfwCreateWindow(1200, 800, "VulkanFoundation", nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::cout << "extensions supported : " << extensionCount << std::endl;
-
-	glm::mat4 matrix;
-	glm::vec4 vec;
-	auto test = matrix * vec;
-
-	while (!glfwWindowShouldClose(window))
+	try {
+		app.Run();
+	}
+	catch (const std::runtime_error& e)
 	{
-		glfwPollEvents();
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
 	}
 
-	glfwDestroyWindow(window);
-	glfwTerminate();
-
-	return 0;
+	return EXIT_SUCCESS;
 }
